5dac6a7efdc51da7a2321086a97f72973f5eae29
augustorib <augustorifra@gmail.com>
2024-01-26 18:40:52 -0300

    feat: main content component creation, fisrt steps

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuBarComponent } from '../menu-bar/menu-bar.component';
+import { MainContentComponent } from '../main-content/main-content.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuBarComponent],
+  imports: [MenuBarComponent, MainContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
