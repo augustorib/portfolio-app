b97b71c64bfb46b6e40577df47f67766945870e8
augustorib <augustorifra@gmail.com>
2024-01-26 16:57:59 -0300

    feat: create home page and menu-bar components

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuBarComponent } from '../menu-bar/menu-bar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
