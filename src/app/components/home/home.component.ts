4cabce102248521c74d09d93854ad8c56a606b7e
augustorib <augustorifra@gmail.com>
2024-01-27 16:47:45 -0300

    feat: few implementations in home page, plus tag adjustment in main-content component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuBarComponent } from '../menu-bar/menu-bar.component';
 import { MainContentComponent } from '../main-content/main-content.component';
+import { SpecialitiesBoxComponent } from '../specialities-box/specialities-box.component';
+import { SectionTitleComponent } from '../section-title/section-title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuBarComponent, MainContentComponent],
+  imports: [MenuBarComponent, MainContentComponent, SpecialitiesBoxComponent, SectionTitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
