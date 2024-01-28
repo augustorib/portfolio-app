b1a7f2f8d0aa49ec4bd0ee60746c73152d30261b
augustorib <augustorifra@gmail.com>
2024-01-28 18:08:36 -0300

    feat: organize new components in home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,13 @@ import { MenuBarComponent } from '../menu-bar/menu-bar.component';
 import { MainContentComponent } from '../main-content/main-content.component';
 import { SpecialitiesBoxComponent } from '../specialities-box/specialities-box.component';
 import { SectionTitleComponent } from '../section-title/section-title.component';
+import { SectionAboutComponent } from '../section-about/section-about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuBarComponent, MainContentComponent, SpecialitiesBoxComponent, SectionTitleComponent],
+  imports: [MenuBarComponent, MainContentComponent, SpecialitiesBoxComponent, SectionTitleComponent,
+  SectionAboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
