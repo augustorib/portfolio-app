06a68a7092eb290312970a8d7e9a4846aaf677c0
augustorib <augustorifra@gmail.com>
2024-02-19 02:29:50 -0300

    feat: portfolio Version 1.0

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialBtnComponent } from '../social-btn/social-btn.component';
 
 @Component({
   selector: 'main-content',
   standalone: true,
-  imports: [],
+  imports: [SocialBtnComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
